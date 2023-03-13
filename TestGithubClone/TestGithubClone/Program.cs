343c1e126a9389d7caa1c144d849734040409745
Lukas Frederiksen <10434647@ucn.dk>
2023-03-13 11:12:17 +0100

    Lukas redder kat

--- TestGithubClone/TestGithubClone/Program.cs
+++ TestGithubClone/TestGithubClone/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Spark kattekilling lige i hovedet!");
+Console.WriteLine("Lukas redder katten fra onde Emil");
